d32a6296a19b725c218b91231884c68388cd0907
Oliveira <9288169+jricardooliveira@users.noreply.github.com>
2023-01-20 16:01:38 +0000

    Update main.go

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
     "fmt"
     "net/http"
+    "github.com/go-redis/redis"
 )
 
 func main() {
@@ -11,5 +12,18 @@ func main() {
 }
 
 func HelloServer(w http.ResponseWriter, r *http.Request) {
+    client := redis.NewClient(&redis.Options{
+        Addr: "pinners-db-redis-ams3-dev-do-user-13352444-0.b.db.ondigitalocean.com:25061",
+        Password: "AVNS_5jbeoQuN0HH-s4_jMFJ",
+        DB: 0,
+    })
+    
+    a := time.Now()
+    
+    err := client.Set(ctx, "key", a, 0).Err()
+    if err != nil {
+        panic(err)
+    }
+    
     fmt.Fprintf(w, "Hello Shit, %s!", r.URL.Path[1:])
 }
