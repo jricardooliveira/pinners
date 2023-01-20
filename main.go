92c2804bbb5510cfeafaed40b4fd248d4b181290
Joao Oliveira <joao.oliveira@tonicapp.com>
2023-01-20 16:47:22 +0000

    Build 1

--- main.go
+++ main.go
@@ -1,29 +1,29 @@
 package main
 
 import (
-    "fmt"
-    "net/http"
-    "github.com/go-redis/redis"
+	"fmt"
+	"github.com/go-redis/redis"
+	"net/http"
+	"time"
 )
 
 func main() {
-    http.HandleFunc("/", HelloServer)
-    http.ListenAndServe(":8080", nil)
+	http.HandleFunc("/", HelloServer)
+	http.ListenAndServe(":8080", nil)
 }
 
 func HelloServer(w http.ResponseWriter, r *http.Request) {
-    client := redis.NewClient(&redis.Options{
-        Addr: "pinners-db-redis-ams3-dev-do-user-13352444-0.b.db.ondigitalocean.com:25061",
-        Password: "AVNS_5jbeoQuN0HH-s4_jMFJ",
-        DB: 0,
-    })
-    
-    a := time.Now()
-    
-    err := client.Set(ctx, "key", a, 0).Err()
-    if err != nil {
-        panic(err)
-    }
-    
-    fmt.Fprintf(w, "Hello Shit, %s!", r.URL.Path[1:])
+	client := redis.NewClient(&redis.Options{
+		Addr:     "pinners-db-redis-ams3-dev-do-user-13352444-0.b.db.ondigitalocean.com:25061",
+		Password: "AVNS_5jbeoQuN0HH-s4_jMFJ",
+		DB:       0,
+	})
+
+	a := time.Now()
+	err := client.Set("key", a, 0).Err()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Fprintf(w, "Hello Shit, %s!", r.URL.Path[1:])
 }
