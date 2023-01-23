10c1b8230513382d9671be88e9d5fd04257ffa2c
Joao Oliveira <joao.oliveira@tonicapp.com>
2023-01-23 14:18:35 +0000

    Build 2

--- go.mod
+++ go.mod
@@ -2,9 +2,13 @@ module example.com
 
 go 1.19
 
-require github.com/go-redis/redis/v9 v9.0.0-rc.2
+require (
+	github.com/go-redis/redis/v8 v8.11.5
+	github.com/google/uuid v1.3.0
+)
 
 require (
 	github.com/cespare/xxhash/v2 v2.1.2 // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
+	github.com/onsi/gomega v1.24.1 // indirect
 )
--- main.go
+++ main.go
@@ -3,7 +3,8 @@ package main
 import (
 	"context"
 	"fmt"
-	"github.com/go-redis/redis/v9"
+	"github.com/go-redis/redis/v8"
+	"github.com/google/uuid"
 	"net/http"
 	"time"
 )
@@ -17,17 +18,40 @@ var ctx = context.Background()
 
 func HelloServer(w http.ResponseWriter, r *http.Request) {
 	client := redis.NewClient(&redis.Options{
-		Addr:     "pinners-db-redis-ams3-dev-do-user-13352444-0.b.db.ondigitalocean.com:25061",
-		Password: "AVNS_5jbeoQuN0HH-s4_jMFJ",
+		Addr: "redis-10835.c55.eu-central-1-1.ec2.cloud.redislabs.com:10835",
+		//Addr: "localhost:6379",
+		Username: "default",
+		Password: "BWz6EQvY7TOrYGYXqWzmqDb1yG5eYUup",
 		DB:       0,
 	})
 
 	a := time.Now()
+	fmt.Println(a)
 
-	err := client.Set(ctx, "key", a, 0).Err()
+	id := uuid.New().String()
+
+	err := client.Set(ctx, id, a, 0).Err()
 	if err != nil {
 		panic(err)
 	}
+
+	/*
+
+		fmt.Println("Ping")
+		client.Ping(ctx)
+		fmt.Println("Pong")
+
+		val2, err := client.Get(ctx, "key2").Result()
+
+		if err == redis.Nil {
+			fmt.Println("key2 does not exist")
+		} else if err != nil {
+			fmt.Println(err)
+		} else {
+			fmt.Println("key2", val2)
+		}
+
+	*/
 
 	fmt.Fprintf(w, "Hello Shit, %s!", r.URL.Path[1:])
 
